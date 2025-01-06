45681d50ef276a478f29ccdd09bef2b29a750b99
bigmistqke <vandijckv@gmail.com>
2025-01-06 20:57:09 +0100

    add traverse.filter and traverse.map + simplify material-extension

--- src/extensions/material.tsx
+++ src/extensions/material.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createMemo, createSignal, For, mapArray } from 'solid-js'
+import { createEffect, createSignal, For } from 'solid-js'
 import * as THREE from 'three'
 import { Mesh } from 'three'
 import { ColorInput, H3, Labelled, List, Select, TextureInput } from '../components'
@@ -6,83 +6,64 @@ import { Extension } from '../types'
 import { traverse } from '../utils/traverse'
 
 export default (): Extension => {
-  const [meshes, setMeshes] = createSignal<Array<Mesh>>([])
-
-  const materials = createMemo(
-    mapArray(meshes, mesh => {
-      const [type, setType] = createSignal<
-        'MeshBasicMaterial' | 'MeshPhongMaterial' | 'MeshStandardMaterial'
-      >(mesh.material.type)
-
-      createEffect(() => {
-        if (type() !== mesh.material.type) {
-          const color = mesh.material.color
-          const map = mesh.material.map
-          mesh.material = new THREE[type()]()
-          mesh.material.color = color
-          mesh.material.map = map
-        }
-      })
-
-      return {
-        name: mesh.name,
-        get material() {
-          return mesh.material
-        },
-        type,
-        setType,
-      }
-    }),
-  )
+  const [meshes, setMeshes] = createSignal<Array<THREE.Object3D>>()
 
   return {
     name: 'Material',
     async setup(state) {
-      createEffect(() => {
-        if (state.gltf) {
-          setMeshes(() => {
-            const meshes = new Array<Mesh>()
-            traverse(state.gltf!.scene, object => {
-              if (object instanceof Mesh) {
-                meshes.push(object)
-              }
-            })
-            return meshes
-          })
-        }
-      })
+      createEffect(() =>
+        setMeshes(() =>
+          state.gltf ? traverse.filter(state.gltf!.scene, object => object instanceof Mesh) : [],
+        ),
+      )
     },
     widget() {
       return (
-        <For each={materials()}>
-          {({ name, material, type, setType }) => (
-            <>
-              <H3>{name}</H3>
-              <List>
-                <Labelled label="type">
-                  <Select
-                    value={type()}
-                    options={['MeshPhongMaterial', 'MeshStandardMaterial', 'MeshBasicMaterial']}
-                    onInput={setType}
-                  />
-                </Labelled>
-                <Labelled label="color">
-                  <ColorInput
-                    value={material.color}
-                    onR={r => (material.color.r = r)}
-                    onG={g => (material.color.g = g)}
-                    onB={b => (material.color.b = b)}
-                  />
-                </Labelled>
-                <Labelled label="map">
-                  <TextureInput
-                    texture={material.map}
-                    onInput={texture => (material.map = texture)}
-                  />
-                </Labelled>
-              </List>
-            </>
-          )}
+        <For each={meshes()}>
+          {mesh => {
+            const [type, setType] = createSignal<
+              'MeshBasicMaterial' | 'MeshPhongMaterial' | 'MeshStandardMaterial'
+            >(mesh.material.type)
+
+            createEffect(() => {
+              if (type() !== mesh.material.type) {
+                const color = mesh.material.color
+                const map = mesh.material.map
+                mesh.material = new THREE[type()]()
+                mesh.material.color = color
+                mesh.material.map = map
+              }
+            })
+
+            return (
+              <>
+                <H3>{mesh.name}</H3>
+                <List>
+                  <Labelled label="type">
+                    <Select
+                      value={type()}
+                      options={['MeshPhongMaterial', 'MeshStandardMaterial', 'MeshBasicMaterial']}
+                      onInput={setType}
+                    />
+                  </Labelled>
+                  <Labelled label="color">
+                    <ColorInput
+                      value={mesh.material.color}
+                      onR={r => (mesh.material.color.r = r)}
+                      onG={g => (mesh.material.color.g = g)}
+                      onB={b => (mesh.material.color.b = b)}
+                    />
+                  </Labelled>
+                  <Labelled label="map">
+                    <TextureInput
+                      texture={mesh.material.map}
+                      onInput={texture => (mesh.material.map = texture)}
+                    />
+                  </Labelled>
+                </List>
+              </>
+            )
+          }}
         </For>
       )
     },
--- src/utils/traverse.ts
+++ src/utils/traverse.ts
@@ -7,3 +7,17 @@ export function traverse(root: Object3D, callback: (object: Object3D) => void) {
   }
   walk(root)
 }
+
+traverse.map = <T>(root: Object3D, callback: (object: Object3D) => T): Array<T> => {
+  const array = new Array<T>()
+  traverse(root, node => array.push(callback(node)))
+  return array
+}
+
+traverse.filter = (root: Object3D, callback: (object: Object3D) => boolean): Array<Object3D> => {
+  const array = new Array<Object3D>()
+  traverse(root, node => {
+    if (callback(node)) array.push(node)
+  })
+  return array
+}
